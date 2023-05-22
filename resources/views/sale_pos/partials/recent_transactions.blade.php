bf2b7abd47ddbbb6a1fae401dfd5ef273dfae595
bruno <bwabwire@onfonmedia.com>
2023-05-22 10:19:17 +0300

    buttons changes

--- resources/views/sale_pos/partials/recent_transactions.blade.php
+++ resources/views/sale_pos/partials/recent_transactions.blade.php
@@ -19,21 +19,27 @@
 				<td>
 					{{ $loop->iteration}}.
 				</td>
+				
 				<td>
 					{{ $transaction->invoice_no }} ({{optional($transaction->contact)->name}})
+				</td>
+				<td>
+					{{ $transaction->created_by }} ({{optional($transaction->contact)->name}})
 				</td>
 				<td class="display_currency">
 					{{ $transaction->final_total }}
 				</td>
 				<td>
 					<a href="{{action('SellPosController@edit', [$transaction->id]).$subtype}}">
-	    				<i class="fas fa-pen text-muted" aria-hidden="true" title="{{__('lang_v1.click_to_edit')}}"></i>
+	    				<i class="fas fa-pen fa-2x" aria-hidden="true" title="{{__('lang_v1.click_to_edit')}}"></i>
 	    			</a>
+
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 	    			
-	    			<a href="{{action('SellPosController@destroy', [$transaction->id])}}" class="delete-sale" style="padding-left: 20px; padding-right: 20px"><i class="fa fa-trash text-danger" title="{{__('lang_v1.click_to_delete')}}"></i></a>
+	    			<!-- <a href="{{action('SellPosController@destroy', [$transaction->id])}}" class="delete-sale" style="padding-left: 20px; padding-right: 20px"><i class="fa fa-trash text-danger" title="{{__('lang_v1.click_to_delete')}}"></i></a> -->
 
 	    			<a href="{{action('SellPosController@printInvoice', [$transaction->id])}}" class="print-invoice-link">
-	    				<i class="fa fa-print text-muted" aria-hidden="true" title="{{__('lang_v1.click_to_print')}}"></i>
+	    				<i class="fa fa-print fa-2x" aria-hidden="true" title="{{__('lang_v1.click_to_print')}}"></i>
 	    			</a>
 				</td>
 			</tr>
